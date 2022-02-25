688059ba0b32ce38d69e9a73d2d2605e8b296ca3
Alex <ajm.dosreis.daponte@gmail.com>
2022-02-25 17:02:50 +0100

    Changed .env to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { Center, Heading } from '@chakra-ui/react'
 export default function Header() {
   return (
     <Center bg="blue.500" p={4} w="100%" color="white">
-      <Heading as="h1">{import.meta.env.VITE_APP_NAME}</Heading>
+      <Heading as="h1">Calendrier liturgique catholique</Heading>
     </Center>
   )
 }
